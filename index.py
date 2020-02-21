be5e17c40bbf114d86c94706cc5b34058d856c73
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-02-21 12:01:04 -0500

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import os,tornado.web,tornado.ioloop
 
 class HomeHTMLHandler(tornado.web.RequestHandler):
     def get(self):
-        if self.request.host=="kelimart.com":
+        if self.request.host=="kelimart.com" or self.request.host=="www.kelimart.com":
             self.set_header('Content-Type', 'text/html')
             with open("/root/maxima/kelimart/index.html", "r") as kelimart_home_html:
                 self.write(kelimart_home_html.read())
@@ -10,10 +10,11 @@ class HomeHTMLHandler(tornado.web.RequestHandler):
 
 class HomeCSSHandler(tornado.web.RequestHandler):
     def get(self):
-        self.set_header('Content-Type', 'text/css')
-        with open("/root/maxima/kelimart/i.css", "r") as kelimart_home_css:
-            self.write(kelimart_home_css.read())
-        kelimart_home_css.close()
+        if self.request.host=="kelimart.com" or self.request.host=="www.kelimart.com":
+            self.set_header('Content-Type', 'text/css')
+            with open("/root/maxima/kelimart/i.css", "r") as kelimart_home_css:
+                self.write(kelimart_home_css.read())
+            kelimart_home_css.close()
 
 class MultiplierImageHandler(tornado.web.RequestHandler):
     def get(self):
@@ -24,7 +25,7 @@ class MultiplierImageHandler(tornado.web.RequestHandler):
 
 class Favicon64Handler(tornado.web.RequestHandler):
     def get(self):
-        if self.request.host=="kelimart.com":
+        if self.request.host=="kelimart.com" or self.request.host=="www.kelimart.com":
             self.set_header('Content-Type', 'image/png')
             with open("/root/maxima/kelimart/images/Faroe_Logo_64.png", "rb") as kelimart_favicon_64:
                 self.write(kelimart_favicon_64.read())
