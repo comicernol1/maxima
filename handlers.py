e31b38afa5c72ba967c6e040f738c316d6ced375
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-03-03 18:46:59 -0500

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -9,72 +9,57 @@ db = mysql.connector.connect(
 )
 mycursor = db.cursor()
 
-class HomeHand(tornado.web.RequestHandler):
+class FranzarHomeHand(tornado.web.RequestHandler):
   def get(self):
-    if self.request.host=="kelimart.com" or self.request.host=="www.kelimart.com":
-      self.set_status(200)
-      self.set_header("Content-Type", "text/html")
-      self.set_header("Access-Control-Allow-Origin", "*")
-      self.set_header("Access-Control-Allow-Headers", "*")
-      self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-      self.set_header("Access-Control-Max-Age", 1000)
-      self.set_header("Access-Control-Allow-Headers", "*")
-      self.render('index.html')
-    else:
-      self.set_status(404)
+    self.set_status(200)
+    self.set_header("Content-Type", "text/html")
+    self.set_header("Access-Control-Allow-Origin", "*")
+    self.set_header("Access-Control-Allow-Headers", "*")
+    self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+    self.set_header("Access-Control-Max-Age", 1000)
+    self.set_header("Access-Control-Allow-Headers", "*")
+    self.render('index.html')
 
-class SignInHand(tornado.web.RequestHandler):
+class FranzarSignInHand(tornado.web.RequestHandler):
   def get(self):
-    if self.request.host=="kelimart.com" or self.request.host=="www.kelimart.com":
-      self.set_status(200)
-      self.set_header("Content-Type", "text/html")
-      self.set_header("Access-Control-Allow-Origin", "*")
-      self.set_header("Access-Control-Allow-Headers", "*")
-      self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-      self.set_header("Access-Control-Max-Age", 1000)
-      self.set_header("Access-Control-Allow-Headers", "*")
-      self.render('signin.html')
-    else:
-      self.set_status(404)
+    self.set_status(200)
+    self.set_header("Content-Type", "text/html")
+    self.set_header("Access-Control-Allow-Origin", "*")
+    self.set_header("Access-Control-Allow-Headers", "*")
+    self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+    self.set_header("Access-Control-Max-Age", 1000)
+    self.set_header("Access-Control-Allow-Headers", "*")
+    self.render('signin.html')
 
     def post(self):
-      if self.request.host=="kelimart.com" or self.request.host=="www.kelimart.com":
-        SignInRequestBody=self.request.body.decode('utf-8')
-        SignInRequestEmail=urllib.parse.unquote(SignInRequestBody[(SignInRequestBody.index("siem=")+5):SignInRequestBody.index("&sipw=")])
-        
-        SignInRequestPasswordPre=urllib.parse.unquote(SignInRequestBody[(SignInRequestBody.index("sipw=")+5):len(SignInRequestBody)])
-        SignInRequestPassword=ph.hash(SignInRequestPasswordPre)
-        SignInRequestDBInsert="INSERT INTO compacc (email, passwd) VALUES ('{0:s}', '{1:s}')".format(SignInRequestEmail, SignInRequestPassword)
-        mycursor.execute(SignInRequestDBInsert)
-        db.commit()
-      else:
-        self.set_status(404)
+      SignInRequestBody=self.request.body.decode('utf-8')
+      SignInRequestEmail=urllib.parse.unquote(SignInRequestBody[(SignInRequestBody.index("siem=")+5):SignInRequestBody.index("&sipw=")])
 
-class SignUpHand(tornado.web.RequestHandler):
+      SignInRequestPasswordPre=urllib.parse.unquote(SignInRequestBody[(SignInRequestBody.index("sipw=")+5):len(SignInRequestBody)])
+      SignInRequestPassword=ph.hash(SignInRequestPasswordPre)
+      SignInRequestDBInsert="INSERT INTO compacc (email, passwd) VALUES ('{0:s}', '{1:s}')".format(SignInRequestEmail, SignInRequestPassword)
+      mycursor.execute(SignInRequestDBInsert)
+      db.commit()
+
+class FranzarSignUpHand(tornado.web.RequestHandler):
   def get(self):
-    if self.request.host=="kelimart.com" or self.request.host=="www.kelimart.com":
-      self.set_status(200)
-      self.set_header("Content-Type", "text/html")
-      self.set_header("Access-Control-Allow-Origin", "*")
-      self.set_header("Access-Control-Allow-Headers", "*")
-      self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-      self.set_header("Access-Control-Max-Age", 1000)
-      self.set_header("Access-Control-Allow-Headers", "*")
-      self.render('signup.html')
-    else:
-      self.set_status(404)
+    self.set_status(200)
+    self.set_header("Content-Type", "text/html")
+    self.set_header("Access-Control-Allow-Origin", "*")
+    self.set_header("Access-Control-Allow-Headers", "*")
+    self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+    self.set_header("Access-Control-Max-Age", 1000)
+    self.set_header("Access-Control-Allow-Headers", "*")
+    self.render('signup.html')
 
   def post(self):
-    if self.request.host=="kelimart.com" or self.request.host=="www.kelimart.com":
-      SignUpRequestBody=self.request.body.decode('utf-8')
-      SignUpRequestEmail=urllib.parse.unquote(SignUpRequestBody[(SignUpRequestBody.index("suem=")+5):SignUpRequestBody.index("&supw=")])
-        
-      SignUpRequestPasswordPre=urllib.parse.unquote(SignUpRequestBody[(SignUpRequestBody.index("supw=")+5):len(SignUpRequestBody)])
-      SignUpRequestPassword=ph.hash(SignUpRequestPasswordPre)
-      SignUpRequestDBInsert="INSERT INTO compacc (email, passwd) VALUES ('{0:s}', '{1:s}')".format(SignUpRequestEmail, SignUpRequestPassword)
-      mycursor.execute(SignUpRequestDBInsert)
-      db.commit()
-    else:
-      self.set_status(404)
+    SignUpRequestBody=self.request.body.decode('utf-8')
+    SignUpRequestEmail=urllib.parse.unquote(SignUpRequestBody[(SignUpRequestBody.index("suem=")+5):SignUpRequestBody.index("&supw=")])
+
+    SignUpRequestPasswordPre=urllib.parse.unquote(SignUpRequestBody[(SignUpRequestBody.index("supw=")+5):len(SignUpRequestBody)])
+    SignUpRequestPassword=ph.hash(SignUpRequestPasswordPre)
+    SignUpRequestDBInsert="INSERT INTO compacc (email, passwd) VALUES ('{0:s}', '{1:s}')".format(SignUpRequestEmail, SignUpRequestPassword)
+    mycursor.execute(SignUpRequestDBInsert)
+    db.commit()
 
 db.close()
