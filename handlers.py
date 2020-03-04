0c227ae9d75ed8942f235ff694458400eed74083
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-03-03 19:07:15 -0500

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -2,64 +2,64 @@ import os,tornado.web,urllib.parse,mysql.connector
 from argon2 import PasswordHasher
 ph = PasswordHasher()
 db = mysql.connector.connect(
-  host="localhost",
-  user="maxima",
-  passwd=str(os.environ["MYSQL_MAXIMA_PASSWD"]),
-  database="franzar"
+    host="localhost",
+    user="maxima",
+    passwd=str(os.environ["MYSQL_MAXIMA_PASSWD"]),
+    database="franzar"
 )
 mycursor = db.cursor()
 
 class HomeHand(tornado.web.RequestHandler):
-  def get(self):
-    self.set_status(200)
-    self.set_header("Content-Type", "text/html")
-    self.set_header("Access-Control-Allow-Origin", "*")
-    self.set_header("Access-Control-Allow-Headers", "*")
-    self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-    self.set_header("Access-Control-Max-Age", 1000)
-    self.set_header("Access-Control-Allow-Headers", "*")
-    self.render('index.html')
+    def get(self):
+        self.set_status(200)
+        self.set_header("Content-Type", "text/html")
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "*")
+        self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+        self.set_header("Access-Control-Max-Age", 1000)
+        self.set_header("Access-Control-Allow-Headers", "*")
+        self.render('index.html')
 
 class SignInHand(tornado.web.RequestHandler):
-  def get(self):
-    self.set_status(200)
-    self.set_header("Content-Type", "text/html")
-    self.set_header("Access-Control-Allow-Origin", "*")
-    self.set_header("Access-Control-Allow-Headers", "*")
-    self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-    self.set_header("Access-Control-Max-Age", 1000)
-    self.set_header("Access-Control-Allow-Headers", "*")
-    self.render('signin.html')
+    def get(self):
+        self.set_status(200)
+        self.set_header("Content-Type", "text/html")
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "*")
+        self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+        self.set_header("Access-Control-Max-Age", 1000)
+        self.set_header("Access-Control-Allow-Headers", "*")
+        self.render('signin.html')
 
     def post(self):
-      SignInRequestBody=self.request.body.decode('utf-8')
-      SignInRequestEmail=urllib.parse.unquote(SignInRequestBody[(SignInRequestBody.index("siem=")+5):SignInRequestBody.index("&sipw=")])
+        SignInRequestBody=self.request.body.decode('utf-8')
+        SignInRequestEmail=urllib.parse.unquote(SignInRequestBody[(SignInRequestBody.index("siem=")+5):SignInRequestBody.index("&sipw=")])
 
-      SignInRequestPasswordPre=urllib.parse.unquote(SignInRequestBody[(SignInRequestBody.index("sipw=")+5):len(SignInRequestBody)])
-      SignInRequestPassword=ph.hash(SignInRequestPasswordPre)
-      SignInRequestDBInsert="INSERT INTO compacc (email, passwd) VALUES ('{0:s}', '{1:s}')".format(SignInRequestEmail, SignInRequestPassword)
-      mycursor.execute(SignInRequestDBInsert)
-      db.commit()
+        SignInRequestPasswordPre=urllib.parse.unquote(SignInRequestBody[(SignInRequestBody.index("sipw=")+5):len(SignInRequestBody)])
+        SignInRequestPassword=ph.hash(SignInRequestPasswordPre)
+        SignInRequestDBInsert="INSERT INTO compacc (email, passwd) VALUES ('{0:s}', '{1:s}')".format(SignInRequestEmail, SignInRequestPassword)
+        mycursor.execute(SignInRequestDBInsert)
+        db.commit()
 
 class SignUpHand(tornado.web.RequestHandler):
-  def get(self):
-    self.set_status(200)
-    self.set_header("Content-Type", "text/html")
-    self.set_header("Access-Control-Allow-Origin", "*")
-    self.set_header("Access-Control-Allow-Headers", "*")
-    self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-    self.set_header("Access-Control-Max-Age", 1000)
-    self.set_header("Access-Control-Allow-Headers", "*")
-    self.render('signup.html')
+    def get(self):
+        self.set_status(200)
+        self.set_header("Content-Type", "text/html")
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "*")
+        self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+        self.set_header("Access-Control-Max-Age", 1000)
+        self.set_header("Access-Control-Allow-Headers", "*")
+        self.render('signup.html')
 
-  def post(self):
-    SignUpRequestBody=self.request.body.decode('utf-8')
-    SignUpRequestEmail=urllib.parse.unquote(SignUpRequestBody[(SignUpRequestBody.index("suem=")+5):SignUpRequestBody.index("&supw=")])
+    def post(self):
+        SignUpRequestBody=self.request.body.decode('utf-8')
+        SignUpRequestEmail=urllib.parse.unquote(SignUpRequestBody[(SignUpRequestBody.index("suem=")+5):SignUpRequestBody.index("&supw=")])
 
-    SignUpRequestPasswordPre=urllib.parse.unquote(SignUpRequestBody[(SignUpRequestBody.index("supw=")+5):len(SignUpRequestBody)])
-    SignUpRequestPassword=ph.hash(SignUpRequestPasswordPre)
-    SignUpRequestDBInsert="INSERT INTO compacc (email, passwd) VALUES ('{0:s}', '{1:s}')".format(SignUpRequestEmail, SignUpRequestPassword)
-    mycursor.execute(SignUpRequestDBInsert)
-    db.commit()
+        SignUpRequestPasswordPre=urllib.parse.unquote(SignUpRequestBody[(SignUpRequestBody.index("supw=")+5):len(SignUpRequestBody)])
+        SignUpRequestPassword=ph.hash(SignUpRequestPasswordPre)
+        SignUpRequestDBInsert="INSERT INTO compacc (email, passwd) VALUES ('{0:s}', '{1:s}')".format(SignUpRequestEmail, SignUpRequestPassword)
+        mycursor.execute(SignUpRequestDBInsert)
+        db.commit()
 
 db.close()
