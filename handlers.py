a806242e9d11d72423f557647d27c2dd0c99bf00
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-03-07 16:58:40 -0500

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -71,7 +71,7 @@ class SignUpHand(tornado.web.RequestHandler):
             db.commit()
             with open("/root/maxima/templates/sign_up/conf_email.html") as SignUpSMPTTemplate_F:
                 SignUpSMTPTemplate=SignUpSMPTTemplate_F.read()
-            SignUpSMTPHeaders="\r\n".join(["from: comicernol@gmail.com","subject: Testing","to:"+SignUpRequestEmail,"mime-version: 1.0","content-type: text/html"])
+            SignUpSMTPHeaders="\r\n".join(["from: comicernol@gmail.com","subject: Verify Your Email - FRANZAR","to:"+SignUpRequestEmail,"mime-version: 1.0","content-type: text/html"])
             SignUpSMTPContent=SignUpSMTPHeaders+"\r\n\r\n"+SignUpSMTPTemplate
             SignUpMail=smtplib.SMTP('smtp.gmail.com',587)
             SignUpMail.ehlo()
