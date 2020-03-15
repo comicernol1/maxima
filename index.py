ce5472f95750bb757bd88303c27ea0bfdea09a6d
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-03-15 17:43:27 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,13 @@
 import os,tornado.web,tornado.ioloop
-from handlers import HomeHand,SignInHand,SignUpHand
+from handlers import HomeHand,SignInHand,SignUpHand,VerifyHand
 from settings import settings
 
 if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/", HomeHand),
         (r"/sign_in/", SignInHand),
-        (r"/sign_up/", SignUpHand)
+        (r"/sign_up/", SignUpHand),
+        (r"/verify/", VerifyHand)
     ], **settings)
 
     app.listen(80)
