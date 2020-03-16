d606cdaa90ec7536309603c2df2e4c415eb33ccc
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-03-16 18:55:03 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,4 +6,7 @@ settings = {
     'template_path': os.path.join(ROOT, "req"),
     'static_path': os.path.join(ROOT, "static"),
     'static_url_prefix': '/static/',
+    "cookie_secret": str(os.environ["COOKIE_SECRET"]),
+    "login_url": "/sign_in/",
+    "xsrf_cookies": True,
 }
