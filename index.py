d2af9d1e965a90b6a6270e9b880e61f9d98492d6
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-03-19 22:43:38 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import os,tornado.web,tornado.ioloop
-from handlers import HomeHand,ContactHand,SignInHand,SignUpHand,VerifyHand
+from handlers import HomeHand,ContactHand,SignInHand,ForgotPWHand,SignUpHand,VerifyHand
 from settings import settings
 
 if __name__ == "__main__":
@@ -7,6 +7,7 @@ if __name__ == "__main__":
         (r"/", HomeHand),
         (r"/contact/", ContactHand),
         (r"/sign_in/", SignInHand),
+        (r"/forgot_password/", ForgotPWHand),
         (r"/sign_up/", SignUpHand),
         (r"/verify/", VerifyHand)
     ], **settings)
