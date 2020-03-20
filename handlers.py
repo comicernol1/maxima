da6a8ad833edcef40496a29cbc37908e17f881ca
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-03-20 18:39:44 -0400

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -34,8 +34,10 @@ def CheckLogin(self):
 class HomeHand(tornado.web.RequestHandler):
     def get(self):
         HomeProductList = ""
-        for i in range(0,1):
-            HomeProductList += "<a style=\"background-image:url(/static/products/"+str(i)+"/0.jpg);\" href=\"/product/"+str(i)+"/\"><div class=\"BPX\"><span><abbr style=\"background:#2d2e32;\"></abbr></span><h6>Product "+str(i)+"</h6><h1>$18.00</h1></div></a>\n"
+        mycursor.execute("SELECT * from products")
+        QueryProductsDict = mycursor.fetchall()
+        for i in range(0,len(QueryProductsDict)):
+            HomeProductList += "<a style=\"background-image:url(/static/products/"+QueryProductsDict[i][0]+"/0.jpg);\" href=\"/product/"+str(i)+"/\"><div class=\"BPX\"><span><abbr style=\"background:#2d2e32;\"></abbr></span><h6>Product "+str(i)+"</h6><h1>$18.00</h1></div></a>\n"
         with open("/root/maxima/req/index.html") as HomeIndex_F:
             HomeIndex = HomeIndex_F.read()
         HomeIndex = HomeIndex.replace("<% Products %>", HomeProductList)
