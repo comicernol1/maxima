4f648c98555c785222f48796e9c3183e97d6e2d6
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-03-22 16:39:34 -0400

    Update index.py

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         (r"/sign_in/reset_password/", handlers.ResetPWHand),
         (r"/sign_up/", handlers.SignUpHand),
         (r"/verify/", handlers.VerifyHand),
-        (r"/terms_and_conditions/", handlers.TermsConditionsHand),
+        (r"/legal/terms_and_conditions/", handlers.TermsConditionsHand),
         (r"/.*", handlers.NotFoundHand)
     ], **settings)
 
