90a8b9d7cc1df6dc2a7796bb6d26a9b5965f4e89
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-03-23 14:35:00 -0400

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -353,7 +353,6 @@ class ResetPWHand(tornado.web.RequestHandler):
         ResetPWErrorIndex = ResetPWErrorIndex.replace("<% Footer %>",FooterHTML)
         
         # Test
-        """
         try:
             ResetPWRequestE = int(self.get_query_argument("e"))
             ResetPWRequestTempID = int(self.get_query_argument("id"))
@@ -396,8 +395,10 @@ class ResetPWHand(tornado.web.RequestHandler):
             else:
                 ResetPWErrorIndex = ResetPWErrorIndex.replace("<% ErrorMsg %>","(R1) Something went wrong. Please click on the link again.")
                 self.write(ResetPWErrorIndex)
-        """
-        self.write(ResetPWErrorIndex)
+        else:
+            # Route to forgot password
+            self.write(ResetPWErrorIndex)
+            
     def post(self):
         # Open Reset Password
         with open("/root/maxima/req/sign_in/reset_pw.html") as ResetPWIndex_F:
