68018e582c7c8bb87ddedb51c63253ddb8766d31
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-03-23 14:42:16 -0400

    Update pub.py

--- pub.py
+++ pub.py
@@ -1,6 +1,9 @@
 import os,subprocess
-MaximaProcessID = subprocess.check_output("pgrep -f ~/maxima/index.py", shell=True).rstrip().decode("utf-8")
-os.system("kill "+MaximaProcessID)
+try:
+    MaximaProcessID = subprocess.check_output("pgrep -f ~/maxima/index.py", shell=True).rstrip().decode("utf-8")
+    os.system("kill "+MaximaProcessID)
+except subprocess.CalledProcessError:
+    pass
 os.system("rm -r maxima")
 os.system("git clone https://www.github.com/comicernol1/maxima.git")
 os.system("python3 ~/maxima/index.py &")
