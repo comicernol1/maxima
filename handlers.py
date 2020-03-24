03705c15b1726619cdd64d64813f4d4e787d80e4
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-03-24 13:08:34 -0400

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,4 +1,4 @@
-import HomeHand
+import os,random,base64,tornado.web,urllib.parse,mysql.connector,smtplib
 from cryptography.fernet import Fernet
 Enc32a = Fernet(base64.b64encode(os.environ["Enc32a"].encode()))
 Enc32b = Fernet(base64.b64encode(os.environ["Enc32b"].encode()))
