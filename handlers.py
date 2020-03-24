ccce945b395e1d438c97c7a972e86aa20329f3ae
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-03-24 17:12:34 -0400

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -644,7 +644,7 @@ class AccountHand(tornado.web.RequestHandler):
             # Set AddressOptions
             AccountAddressOptions = ""
             for AFi in range(0,len(AccountAddressesFetch)):
-                AccountAddressOptions += "<option value=\""+str(AccountAddressesFetch[AFi][0])+"\">"+str(AccountAddressesFetch[AFi][0])+"</option>\n"
+                AccountAddressOptions += "<option value=\""+str(AccountAddressesFetch[AFi][0])+"\">"+str(AccountAddressesFetch[AFi][1])+"</option>\n"
             
             # Open Account
             with open("/root/maxima/req/account/index.html") as AccountIndex_F:
