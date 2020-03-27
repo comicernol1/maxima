51629f33c12f7edfdac0dd90605cd2cf69485b0b
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-03-27 15:55:56 -0400

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -11,19 +11,7 @@ db = mysql.connector.connect(
 )
 mycursor = db.cursor()
         
-def CheckLogin(self):
-    if self.get_secure_cookie("Fu") and self.get_secure_cookie("Ft"):
-        UserInfoFu = self.get_secure_cookie("Fu")
-        UserInfoFt = self.get_secure_cookie("Ft")
-        UserInfoLoginQuery = "SELECT * from compacc where userid='{0:d}' and token='{1:d}'".format(int(UserInfoFu),int(UserInfoFt))
-        mycursor.execute(UserInfoLoginQuery)
-        UserInfoLoginFetch = mycursor.fetchone()
-        if UserInfoLoginFetch:
-            return True
-        else:
-            return False
-    else:
-        return False
+from udf import CheckLogin
 
 def ServePage(self,pageloc):
     # Define Basics
