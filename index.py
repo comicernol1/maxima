b13eac9b7c4db7b24c59f63a418cbe049b5a01e7
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-03-27 16:03:33 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,19 +1,20 @@
-import handlers,os,tornado.web,tornado.ioloop
+import os,tornado.web,tornado.ioloop
+from handlers import *
 from settings import settings
 
 if __name__ == "__main__":
     app = tornado.web.Application([
-        (r"/", handlers.HomeHand),
-        (r"/contact/", handlers.ContactHand),
-        (r"/sign_in/", handlers.SignInHand),
-        (r"/sign_in/forgot_password/", handlers.ForgotPWHand),
-        (r"/sign_in/reset_password/", handlers.ResetPWHand),
-        (r"/sign_up/", handlers.SignUpHand),
-        (r"/verify/", handlers.VerifyHand),
-        (r"/account/", handlers.AccountHand),
-        (r"/product/.*", handlers.ProductHand),
-        (r"/legal/terms_and_conditions/", handlers.TermsConditionsHand),
-        (r"/.*", handlers.NotFoundHand)
+        (r"/", HomeHand),
+        (r"/contact/", ContactHand),
+        (r"/sign_in/", SignInHand),
+        (r"/sign_in/forgot_password/", ForgotPWHand),
+        (r"/sign_in/reset_password/", ResetPWHand),
+        (r"/sign_up/", SignUpHand),
+        (r"/verify/", VerifyHand),
+        (r"/account/", AccountHand),
+        (r"/product/.*", ProductHand),
+        (r"/legal/terms_and_conditions/", TermsConditionsHand),
+        (r"/.*", NotFoundHand)
     ], **settings)
 
     app.listen(80)
