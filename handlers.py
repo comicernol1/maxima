04eb9988c4c9ab082ec0fa1501b42246ae05119f
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-03-31 23:38:11 -0400

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -29,7 +29,7 @@ class HomeHand(tornado.web.RequestHandler):
             ReturnProductColoursDict = ""
             for Ci in range(0,len(QueryProductColoursDict)):
                 if str(QueryProductColoursFetch[Ci][0]) != QueryProductsDefaultColour:
-                ReturnProductColoursDict += "<abbr style=\"background:#"+QueryProductColoursDict["Hex"][Ci]+";\" title=\""+QueryProductColoursDict["Name"][Ci]+"\" s=\"n\"></abbr>"
+                    ReturnProductColoursDict += "<abbr style=\"background:#"+QueryProductColoursDict["Hex"][Ci]+";\" title=\""+QueryProductColoursDict["Name"][Ci]+"\" s=\"n\"></abbr>"
             HomeProductList += "<a style=\"background-image:url(/static/product/"+QueryProductsID+"/0.jpg);\" href=\"/product/"+QueryProductsID+"/\"><div class=\"BPX\"><span><abbr style=\"background:#"+QueryProductsDefaultColour+";\" title=\""+QueryProductsDefaultColour+"\" s=\"y\"></abbr>"+ReturnProductColoursDict+"</span><h6>"+str(QueryProductsDict[i][1])+"</h6>"+QueryProductsPriceSet+"</div></a>\n"
         
         # Open
