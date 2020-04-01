0401238af1739bf8b154a038808c54d90ca5786d
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-01 15:05:25 -0400

    Update ajaxHand.py

--- ajaxHand.py
+++ ajaxHand.py
@@ -11,6 +11,7 @@ class AddToCartAjax(tornado.web.RequestHandler):
                 ATCRequestID = ATCRequest[(ATCRequest.index("id=")+3):ATCRequest.index("&qty=")]
                 ATCRequestQty = ATCRequest[(ATCRequest.index("&qty=")+5):len(ATCRequest)]
                 print(UserInfoFu)
+                return "Success"
             else:
                 print("Error A")
         else:
