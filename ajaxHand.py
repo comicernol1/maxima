e377b98e0b43aafbb1c2dec8edddf0d07890ffa2
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-01 15:04:22 -0400

    Update ajaxHand.py

--- ajaxHand.py
+++ ajaxHand.py
@@ -6,7 +6,7 @@ class AddToCartAjax(tornado.web.RequestHandler):
     def post(self):
         ATCRequest = urllib.parse.unquote(self.request.body.decode('utf-8'))
         if self.get_secure_cookie("Fu"):
-            UserInfoFu = self.get_secure_cookie("Fu")
+            UserInfoFu = self.get_secure_cookie("Fu").decode('utf-8')
             if ATCRequest.find("id=") >= 0 and ATCRequest.find("&qty=") >= 0:
                 ATCRequestID = ATCRequest[(ATCRequest.index("id=")+3):ATCRequest.index("&qty=")]
                 ATCRequestQty = ATCRequest[(ATCRequest.index("&qty=")+5):len(ATCRequest)]
