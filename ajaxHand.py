37decc5df8d367c773cae6db60cb21b298c80e00
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-01 15:42:44 -0400

    Update ajaxHand.py

--- ajaxHand.py
+++ ajaxHand.py
@@ -6,10 +6,13 @@ class AddToCartAjax(tornado.web.RequestHandler):
     def post(self):
         ATCRequest = urllib.parse.unquote(self.request.body.decode('utf-8'))
         if self.get_secure_cookie("Fu"):
-            UserInfoFu = self.get_secure_cookie("Fu").decode('utf-8')
+            UserInfoFu = int(self.get_secure_cookie("Fu"))
             if ATCRequest.find("id=") >= 0 and ATCRequest.find("&qty=") >= 0:
-                ATCRequestID = ATCRequest[(ATCRequest.index("id=")+3):ATCRequest.index("&qty=")]
-                ATCRequestQty = ATCRequest[(ATCRequest.index("&qty=")+5):len(ATCRequest)]
+                ATCRequestID = int(ATCRequest[(ATCRequest.index("id=")+3):ATCRequest.index("&qty=")])
+                ATCRequestQty = int(ATCRequest[(ATCRequest.index("&qty=")+5):len(ATCRequest)])
+                UserCartQuery = "INSERT INTO cart (uid,pid,qty) VALUES({0:d},{1:d},{2:d})".format(UserInfoFu,ATCRequestID,ATCRequestQty)
+                mycursor.execute(UserCartQuery)
+                db.commit()
                 self.write("A")
             else:
                 self.write("E_A")
