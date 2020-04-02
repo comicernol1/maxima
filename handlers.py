4e8d94d984770a940c874d8106b4f975289dfeaf
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-01 21:11:34 -0400

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -565,7 +565,7 @@ class CartHand(tornado.web.RequestHandler):
                 UserCartItem_ImgLink = "/static/product/"+UserCartItem_ID+"/0.jpg"
             else:
                 UserCartItem_ImgLink = "/static/product/missing.jpg"
-            UserCartItems += "<div class=\"CIt\" style=\"top:"+str(i*300)+"px;\"><div class=\"CIi\" style=\"background-image:url("+UserCartItem_ImgLink+");\"></div><h1>"+FindProduct(UserCartItem_ID)["Name"]+"</h1><input type=\"number\" value=\""+str(UserCartList[i][1])+"\"></div>\n"
+            UserCartItems += "<div class=\"CIt\" style=\"top:"+str(i*300)+"px;\"><input type=\"number\" value=\""+str(UserCartList[i][1])+"\"><div class=\"CIi\" style=\"background-image:url("+UserCartItem_ImgLink+");\"></div><h1>"+FindProduct(UserCartItem_ID)["Name"]+"</h1></div>\n"
         CartIndex = CartIndex.replace("<% Cart %>",UserCartItems)
         self.write(CartIndex)
 
