5a5cd7290a5e25a93ddc5900cd8643327f173ef1
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-03 17:34:38 -0400

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -427,7 +427,7 @@ class VerifyHand(tornado.web.RequestHandler):
             VerifyIndex = ServePage(self,"/sign_up/verified.html")
             VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>",str(VerifyTmpCode))
             self.write(VerifyIndex)
-        except HTTPError:
+        except tornado.web.MissingArgumentError:
             self.write("Err 400")
         else:
             self.write("Something went wrong")
