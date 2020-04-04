5dfe361699ba1b15b3bb8b1c1a6b422829fc07a1
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-04 17:18:30 -0400

    Update TestingHand.py

--- TestingHand.py
+++ TestingHand.py
@@ -2,7 +2,9 @@ from udf import *
 
 class handler(tornado.web.RequestHandler):
     def get(self):
-        self.set_cookie("Fu","0123456789")
+        VerifyIndex = ServePage(self,"/sign_up/verified.html")
+        VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","Your Email has been verified")
+        self.write(VerifyIndex)
     
     def post(self):
         SetCookie(self)
