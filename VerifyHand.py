70930a3242c46fb151ae54b5f3393d376560a89b
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-04 18:02:01 -0400

    Update VerifyHand.py

--- VerifyHand.py
+++ VerifyHand.py
@@ -20,13 +20,13 @@ class handler(tornado.web.RequestHandler):
                         VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","<div id=\"rg_block\" hg=\"uc\"><h1>Welcome to Franzar!</h1><div id=\"suc_i\"></div><h3>As a sign of appreciation for making it this far, we've added a FREE tailor's ruler to your cart. You'll need this if you want to order custom tailored clothes from us. To see your cart, you can click the button below or the shopping cart icon in the top-right corner.</h3><a href=\"/cart/\"><button class=\"rgsb\" id=\"Vuc_L\">Open Cart</button></a></div>")
                         self.write(VerifyIndex)
                     else:
-                        VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","(V1) This account could not be found")
+                        VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","<div id=\"rg_block\" hg=\"ue\">(V1) This account could not be found</div>")
                         self.write(VerifyIndex)
                 else:
-                    VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","This Email is already verified")
+                    VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","<div id=\"rg_block\" hg=\"ue\">This Email is already verified</div>")
                     self.write(VerifyIndex)
             else:
-                VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","(V2) This account could not be found")
+                VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","<div id=\"rg_block\" hg=\"ue\">(V2) This account could not be found</div>")
                 self.write(VerifyIndex)
         
         if self.get_cookie("Fu"):
@@ -41,7 +41,7 @@ class handler(tornado.web.RequestHandler):
                     VerifyEmail(UserInfoFu,VerifyTmpCode)
                 else:
                     VerifyIndex = ServePage(self,"/sign_up/verified.html")
-                    VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","This link is invalid")
+                    VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","<div id=\"rg_block\" hg=\"ue\">This link is invalid</div>")
                     self.write(VerifyIndex)
         else:
             self.redirect("/sign_in/")
