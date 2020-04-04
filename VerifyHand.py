cb90c72f3ada313cab0d86231c04e096d7972b01
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-04 17:19:01 -0400

    Update VerifyHand.py

--- VerifyHand.py
+++ VerifyHand.py
@@ -45,43 +45,6 @@ class handler(tornado.web.RequestHandler):
                     self.write(VerifyIndex)
         else:
             self.redirect("/sign_in/")
-        """
-        VerifyIndex = ServePage(self,"/sign_up/verified.html")
-        def VerifyEmail(uid,tmpcode):
-            VENewToken = random.randint(1000000000,9999999999)
-            self.set_cookie("Ft",str(VENewToken))
-            VERequestDBUpdate = "UPDATE compacc SET veremail=1,token='{0:d}' WHERE userid='{1:d}' and tmpcode='{2:d}'".format(VENewToken,int(uid),int(tmpcode))
-            mycursor.execute(VERequestDBUpdate)
-            if mycursor.rowcount >= 1:
-                return True
-            else:
-                return False
-            db.commit()
-        
-        try:
-            if self.get_cookie("Fu"):
-                VerifyTmpCode = int(self.get_query_argument("e"))
-                self.set_cookie("Fv",str(VerifyTmpCode))
-                if VerifyEmail(int(self.get_cookie("Fu")),VerifyTmpCode):
-                    VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","(Vr1) Your email has been verified")
-                    self.write(VerifyIndex)
-                else:
-                    VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","(Ve1) This account could not be found")
-                    self.write(VerifyIndex)
-            else:
-                self.redirect("/sign_in/")
-        except tornado.web.MissingArgumentError:
-            if self.get_cookie("Fu") and self.get_cookie("Fv"):
-                if VerifyEmail(int(self.get_cookie("Fu")),int(self.get_cookie("Fv"))):
-                    VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","(Vr2) Your email has been verified")
-                    self.write(VerifyIndex)
-                else:
-                    VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","(Ve2) This account could not be found")
-                    self.write(VerifyIndex)
-            else:
-                VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","(V1) Something went wrong")
-                self.write(VerifyIndex)
-        """
     
     def post(self):
         SetCookie(self)
