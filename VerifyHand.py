dd52d6eee19b8ccdd1b6d6875a7230aa858dfea9
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-04 18:20:47 -0400

    Update VerifyHand.py

--- VerifyHand.py
+++ VerifyHand.py
@@ -17,6 +17,9 @@ class handler(tornado.web.RequestHandler):
                     mycursor.execute(VE_RequestDBUpdate)
                     db.commit()
                     if mycursor.rowcount >= 1:
+                        VE_AddRulerToCart = "INSERT INTO cart (uid,pid,qty) VALUES('{0:d}','11111110',1)".format(VE_uid)
+                        mycursor.execute(VE_AddRulerToCart)
+                        db.commit()
                         VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","<div id=\"rg_block\" hg=\"uc\"><h1>Welcome to Franzar!</h1><div id=\"suc_i\"></div><h3>As a sign of appreciation for making it this far, we've added a FREE tailor's ruler to your cart. You'll need this if you want to order custom tailored clothes from us. To see your cart, you can click the button below or the shopping cart icon in the top-right corner.</h3><a href=\"/cart/\"><button class=\"rgsb\" id=\"Vuc_L\">Open Cart</button></a></div>")
                         self.write(VerifyIndex)
                     else:
