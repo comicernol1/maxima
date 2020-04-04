2e60c473a6b3e8b3109abd087194635856e81a4e
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-03 22:37:30 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,26 +1,33 @@
 import os,tornado.web,tornado.ioloop
-from handlers import *
-from ajaxHand import *
+import HomeHand,ContactHand
+import SignInHand,ForgotPWHand,ResetPWHand
+import SignUpHand,VerifyHand
+import AccountHand
+import ProductHand,CartHand
+import TermsConditionsHand,CounterfeitHand
+import AddToCartAjax,RefreshCartAjax
+import TestingHand
+import NotFoundHand
 from settings import settings
 
 if __name__ == "__main__":
     app = tornado.web.Application([
-        (r"/", HomeHand),
-        (r"/contact/", ContactHand),
-        (r"/sign_in/", SignInHand),
-        (r"/sign_in/forgot_password/", ForgotPWHand),
-        (r"/sign_in/reset_password/", ResetPWHand),
-        (r"/sign_up/", SignUpHand),
-        (r"/verify/", VerifyHand),
-        (r"/account/", AccountHand),
-        (r"/product/.*", ProductHand),
-        (r"/cart/", CartHand),
-        (r"/legal/terms_and_conditions/", TermsConditionsHand),
-        (r"/report_a_counterfeit/", CounterfeitHand),
-        (r"/add_to_cart/", AddToCartAjax),
-        (r"/refresh_cart/", RefreshCartAjax),
-        (r"/test/", TestingHand),
-        (r"/.*", NotFoundHand)
+        (r"/", HomeHand.handler),
+        (r"/contact/", ContactHand.handler),
+        (r"/sign_in/", SignInHand.handler),
+        (r"/sign_in/forgot_password/", ForgotPWHand.handler),
+        (r"/sign_in/reset_password/", ResetPWHand.handler),
+        (r"/sign_up/", SignUpHand.handler),
+        (r"/verify/", VerifyHand.handler),
+        (r"/account/", AccountHand.handler),
+        (r"/product/.*", ProductHand.handler),
+        (r"/cart/", CartHand.handler),
+        (r"/legal/terms_and_conditions/", TermsConditionsHand.handler),
+        (r"/report_a_counterfeit/", CounterfeitHand.handler),
+        (r"/add_to_cart/", AddToCartAjax.handler),
+        (r"/refresh_cart/", RefreshCartAjax.handler),
+        (r"/test/", TestingHand.handler),
+        (r"/.*", NotFoundHand.handler)
     ], **settings)
 
     app.listen(80)
