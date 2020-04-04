1fb634acfa1cf06c9b4bb3fc9307ce8d7b32807e
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-03 22:41:29 -0400

    Update ResetPWHand.py

--- ResetPWHand.py
+++ ResetPWHand.py
@@ -52,3 +52,50 @@ class handler(tornado.web.RequestHandler):
                     self.write(ResetPWMsgIndex)
             except:
                 self.redirect("/sign_in/forgot_password/")
+            
+    def post(self):
+        # Open
+        ResetPWIndex = ServePage(self,"/sign_in/reset_pw.html")
+        ResetPWMsgIndex = ServePage(self,"/sign_in/reset_pw_msg.html")
+        
+        # Test
+        if self.get_cookie("Fu") != "":
+            ResetPWRequestBody = urllib.parse.unquote(self.request.body.decode('utf-8'))
+            ResetPWCookieFu = int(self.get_cookie("Fu"))
+            if ResetPWRequestBody.find("rppw=") >= 0 and ResetPWRequestBody.find("rppa=") >= 0:
+                ResetPWRequestNewPWPre = ResetPWRequestBody[(ResetPWRequestBody.index("rppw=")+5):ResetPWRequestBody.index("&rppa=")]
+                ResetPWRequestNewPWAgain = ResetPWRequestBody[(ResetPWRequestBody.index("rppa=")+5):len(ResetPWRequestBody)]
+                ResetPWRequestDBSelectCode = "SELECT email FROM compacc WHERE userid='{0:d}'".format(ResetPWCookieFu)
+                mycursor.execute(ResetPWRequestDBSelectCode)
+                QueryIDPre = mycursor.fetchone()
+                if QueryIDPre:
+                    if ResetPWRequestNewPWPre == ResetPWRequestNewPWAgain:
+                        if len(ResetPWRequestNewPWPre) >= 8:
+                            ResetPWRequestNewPW = Enc32a.encrypt(ResetPWRequestNewPWPre.encode()).decode('utf-8')
+                            ResetPWRequestDBUpdate = "UPDATE compacc SET passwd='{0:s}' WHERE userid='{1:d}'".format(ResetPWRequestNewPW,ResetPWCookieFu)
+                            mycursor.execute(ResetPWRequestDBUpdate)
+                            db.commit()
+                            ResetPWMsgIndex = ResetPWMsgIndex.replace("<% Msg %>","Your password has been changed")
+                            self.write(ResetPWMsgIndex)
+                        else:
+                            ResetPWIndex = ResetPWIndex.replace("<% Email %>",str(QueryIDPre[0]))
+                            ResetPWIndex = ResetPWIndex.replace("<% ShowError %>","block")
+                            ResetPWIndex = ResetPWIndex.replace("<% ErrorMsg %>","Password must be 8 characters or more")
+                            self.write(ResetPWIndex)
+                    else:
+                        ResetPWIndex = ResetPWIndex.replace("<% Email %>",str(QueryIDPre[0]))
+                        ResetPWIndex = ResetPWIndex.replace("<% ShowError %>","block")
+                        ResetPWIndex = ResetPWIndex.replace("<% ErrorMsg %>","Passwords must match")
+                        self.write(ResetPWIndex)
+                else:
+                    ResetPWMsgIndex = ResetPWMsgIndex.replace("<% Msg %>","We can't find an account matching this link.")
+                    self.write(ResetPWMsgIndex)
+            else:
+                ResetPWMsgIndex = ResetPWMsgIndex.replace("<% Msg %>","(R1) Something went wrong")
+                self.write(ResetPWMsgIndex)
+        else:
+            if SetCookie(self):
+                pass
+            else:
+                ResetPWMsgIndex = ResetPWMsgIndex.replace("<% Msg %>","(R2) Something went wrong")
+                self.write(ResetPWMsgIndex)
