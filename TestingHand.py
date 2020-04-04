0eefb53917d00c0a59f8f36a4b6a7f950ec06810
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-04 17:21:06 -0400

    Update TestingHand.py

--- TestingHand.py
+++ TestingHand.py
@@ -3,7 +3,7 @@ from udf import *
 class handler(tornado.web.RequestHandler):
     def get(self):
         VerifyIndex = ServePage(self,"/sign_up/verified.html")
-        VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","Your Email has been verified")
+        VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","<div id=\"rg_block\" hg=\"uc\"><h1>Welcome to Franzar!</h1></div>")
         self.write(VerifyIndex)
     
     def post(self):
