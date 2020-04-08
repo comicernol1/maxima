1dc0ce5d54d2504557512acc7e67fb736b60e670
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-08 13:24:29 -0400

    Update update.py

--- update.py
+++ update.py
@@ -1,1 +1,7 @@
-
+import os
+os.system("sudo apt-get upgrade")
+os.system("sudo apt upgrade")
+os.system("sudo apt-get update")
+os.system("sudo apt update")
+os.system("sudo apt-get autoremove")
+os.system("sudo apt autoremove")
