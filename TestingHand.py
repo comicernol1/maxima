fa1bb95518042ab34cb8d33656b084f22ccecc6f
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-08 21:47:10 -0400

    Update TestingHand.py

--- TestingHand.py
+++ TestingHand.py
@@ -2,7 +2,7 @@ from udf import *
 
 class handler(tornado.web.RequestHandler):
     def get(self):
-        print(tornado.web.request.headers.get("Accept-Language"))
+        print(RequestHandler.request.headers.get("Accept-Language"))
         # VerifyIndex = ServePage(self,"/sign_up/verified.html",False)
         # VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","<div id=\"rg_block\" hg=\"ue\">This Email is already verified</div>")
         # self.write(VerifyIndex)
