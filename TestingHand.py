756de2e7f7af6b58d6e6b27193993dbff2bccd7b
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-08 21:27:18 -0400

    Update TestingHand.py

--- TestingHand.py
+++ TestingHand.py
@@ -2,9 +2,10 @@ from udf import *
 
 class handler(tornado.web.RequestHandler):
     def get(self):
-        VerifyIndex = ServePage(self,"/sign_up/verified.html",False)
-        VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","<div id=\"rg_block\" hg=\"ue\">This Email is already verified</div>")
-        self.write(VerifyIndex)
+        self.write(self.get_browser_locale())
+        # VerifyIndex = ServePage(self,"/sign_up/verified.html",False)
+        # VerifyIndex = VerifyIndex.replace("<% VerificationMsg %>","<div id=\"rg_block\" hg=\"ue\">This Email is already verified</div>")
+        # self.write(VerifyIndex)
     
     def post(self):
         SetCookie(self)
