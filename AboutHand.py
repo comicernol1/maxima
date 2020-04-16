1999a6fb04fc2646be28e81b28b343cb61ff77d2
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-16 16:59:20 -0400

    Update AboutHand.py

--- AboutHand.py
+++ AboutHand.py
@@ -3,7 +3,7 @@ from udf import *
 class handler(tornado.web.RequestHandler):
     def get(self):
         # Open
-        HomeIndex = ServePage(self,"/index.html",False)
+        HomeIndex = ServePage(self,"/about/index.html",False)
         self.write(HomeIndex)
         
     def post(self):
