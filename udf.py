1a3f2b3849ab0c65bc86864db0a3a357eef46301
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-16 18:11:35 -0400

    Update udf.py

--- udf.py
+++ udf.py
@@ -214,7 +214,7 @@ def ServePage(self,pageloc,ForceLogin):
     return PageIndex
 
 def SetCookie(self,cookie_name,cookie_value):
-    self.set_cookie(cookie_name,cookie_value)
+    self.set_cookie(str(cookie_name),str(cookie_value))
 
 def FindAddress(adid):
     try:
