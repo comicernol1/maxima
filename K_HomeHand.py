38973d32ccbc566476205894d14942ab3c208b05
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-16 18:09:08 -0400

    Update K_HomeHand.py

--- K_HomeHand.py
+++ K_HomeHand.py
@@ -1,6 +1,7 @@
 from udf import *
 
 def handler(self):
+    SetCookie(self,"FF","Hello World")
     # Generate Products List
     HomeProductList = ""
     mycursor.execute("SELECT id,ttl,price_"+UserCurrency.lower()+",discount,colour,colour_name FROM products WHERE disp=1 GROUP BY left(id,7)")
