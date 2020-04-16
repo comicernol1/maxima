d8bef2e697630243b5d2d5712c1ce28e778f1182
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-16 16:54:26 -0400

    Create about.py

---
+++ about.py
@@ -0,0 +1,10 @@
+from udf import *
+
+class handler(tornado.web.RequestHandler):
+    def get(self):
+        # Open
+        HomeIndex = ServePage(self,"/index.html",False)
+        self.write(HomeIndex)
+        
+    def post(self):
+        SetCookie(self)
