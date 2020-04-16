3aa7f33b808b66c558d4225cb7467eed30afa403
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-16 17:46:58 -0400

    Update MaximaBranch.py

--- MaximaBranch.py
+++ MaximaBranch.py
@@ -4,10 +4,10 @@ import F_HomeHand
 
 class handler(tornado.web.RequestHandler):
     def get(self):
-        if self.request.host == "kelimart.com" || self.request.host == "www.kelimart.com":
+        if self.request.host == "kelimart.com" or self.request.host == "www.kelimart.com":
             if self.request.path == "/":
                 K_HomeHand.handler(self)
-        elif self.request.host == "franzar.com" || self.request.host == "www.franzar.com":
+        elif self.request.host == "franzar.com" or self.request.host == "www.franzar.com":
             if self.request.path == "/":
                 F_HomeHand.handler(self)
         else:
