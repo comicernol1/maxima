ab5bea3cfde88e04f5b36a3bfc8c284b712352f2
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-16 18:53:07 -0400

    Update udf.py

--- udf.py
+++ udf.py
@@ -215,7 +215,7 @@ def ServePage(self,pageloc,ForceLogin):
     return PageIndex
 
 def CreateCookie(self,cookie_name,cookie_value):
-    a = datetime.date(datetime.now().total_seconds()+1000*60*60*24*365)
+    a = datetime.date(datetime.today().timestamp()+1000)
     self.set_cookie("FF","HelloWorld","kelimart.com",a,"/")
     # self.set_header("Set-Cookie",str(cookie_name)+"="+str(cookie_value)+";")
 
