583b6d538fe764c2a21fbbbde002aa6fb0e35fce
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-16 23:08:07 -0400

    Update udf.py

--- udf.py
+++ udf.py
@@ -218,7 +218,7 @@ def CreateCookie(self,cookie_name: str,cookie_value: str,cookie_expires: int,*ar
         ExpiresDateString = ""
         
     # self.set_cookie(str(cookie_name),str(cookie_value),RequestedHostName,ExpiresDateString,"/",SameSite="Strict")
-    self.set_cookie('trakr', 'email', httponly=True)
+    self.set_cookie('trakr', 'email', samesite='Strict', httponly=True)
 
 def FindAddress(adid):
     try:
