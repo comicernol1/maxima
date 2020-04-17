69592aabcaad4eea440a699b1b789817512fa82e
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-16 21:24:14 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import os,tornado.web,tornado.ioloop
 from tornado.routing import HostMatches
-from .. import HomeHand
+import ../maxima/kelimart/HomeHand
 from settings import settings
 
 if __name__ == "__main__":
