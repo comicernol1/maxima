b4459247a7d403fcc9e8f8cdb96f66a557eb1e99
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-16 23:04:39 -0400

    Update udf.py

--- udf.py
+++ udf.py
@@ -218,7 +218,7 @@ def CreateCookie(self,cookie_name: str,cookie_value: str,cookie_expires: int,*ar
         ExpiresDateString = ""
         
     # self.set_cookie(str(cookie_name),str(cookie_value),RequestedHostName,ExpiresDateString,"/",SameSite="Strict")
-    self.set_secure_cookie('name': 'session_cookie','value': 'session_cookie_value','httponly': True,'expires_days': None,'samesite': 'Strict')
+    self.set_secure_cookie("session_id", "session_key", samesite="None")
 
 def FindAddress(adid):
     try:
