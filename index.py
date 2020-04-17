fdcb318261016abc2b652c8fc28683cf6308bd9c
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-16 20:56:05 -0400

    Update index.py

--- index.py
+++ index.py
@@ -2,9 +2,15 @@ import os,tornado.web,tornado.ioloop,MaximaBranch
 from settings import settings
 
 if __name__ == "__main__":
-    app = tornado.web.Application(r'(localhost|127\.0\.0\.1)', [
+    """
+    app = tornado.web.Application([
         (r'/.*', MaximaBranch.handler)
     ], **settings)
+    """
+    app = Application([
+    (HostMatches(r'(localhost|127\.0\.0\.1)'),
+        [('/.*', MaximaBranch.handler)]),
+    ])
 
     app.listen(80)
     tornado.ioloop.IOLoop.current().start()
