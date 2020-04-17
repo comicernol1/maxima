4d35a3b9ecb10f6b6262bd5ff60ad307c60bd68a
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-16 21:05:38 -0400

    Update F_HomeHand.py

--- F_HomeHand.py
+++ F_HomeHand.py
@@ -1,1 +1,10 @@
+from udf import *
 
+class handler(tornado.web.RequestHandler):
+    def get(self):
+        # Open
+        HomeIndex = ServePage(self,"/index.html",False)
+        self.write(HomeIndex)
+
+    def post(self):
+        AcceptCookies(self)
