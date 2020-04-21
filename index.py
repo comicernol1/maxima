5eb11117e7adb546f2b255c672a7577971a5e972
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-21 09:57:15 -0400

    Update index.py

--- index.py
+++ index.py
@@ -4,60 +4,64 @@ import tornado.ioloop
 from tornado.routing import HostMatches
 from settings import settings
 
-import kelimart.AboutHand
-import kelimart.AccountHand
-import kelimart.AddToCartAjax
-import kelimart.CartHand
-import kelimart.ChangeLocalesAjax
-import kelimart.ContactHand
-import kelimart.ForgotPWHand
+import franzar.AboutHand
+import franzar.AccountHand
+import franzar.AddToCartAjax
+import franzar.CartHand
+import franzar.ChangeLocalesAjax
+import franzar.ContactHand
+import franzar.ForgotPWHand
+import franzar.HomeHand
+import franzar.InfoHand
+import franzar.ProductHand
+import franzar.RefreshCartAjax
+import franzar.ResetPWHand
+import franzar.SignInHand
+import franzar.SignUpHand
+import franzar.TestingHand
+import franzar.VerifyHand
+
 import kelimart.HomeHand
-import kelimart.InfoHand
-import kelimart.ProductHand
-import kelimart.RefreshCartAjax
-import kelimart.ResetPWHand
-import kelimart.SignInHand
-import kelimart.SignUpHand
-import kelimart.TestingHand
-import kelimart.VerifyHand
 
-import franzar.F_HomeHand
+class RedirectRemoveWWW_Franzar(tornado.web.RequestHandler):
+    def prepare(self):
+        self.redirect("https://franzar.com"+self.request.uri)
 
-class RedirectRemoveWWW(tornado.web.RequestHandler):
+class RedirectRemoveWWW_Kelimart(tornado.web.RequestHandler):
     def prepare(self):
         self.redirect("https://kelimart.com"+self.request.uri)
 
 if __name__ == "__main__":
     app = tornado.web.Application([
         # (HostMatches(r'(localhost|127\.0\.0\.1)'), []),
-        (HostMatches("kelimart.com"), [
-            (r"/about_us/", kelimart.AboutHand.handler),
-            (r"/account/", kelimart.AccountHand.handler),
-            (r"/add_to_cart/", kelimart.AddToCartAjax.handler),
-            (r"/cart/", kelimart.CartHand.handler),
-            (r"/account/locales/", kelimart.ChangeLocalesAjax.handler),
-            (r"/contact/", kelimart.ContactHand.handler),
-            (r"/sign_in/forgot_password/", kelimart.ForgotPWHand.handler),
-            (r"/", kelimart.HomeHand.handler),
-            (r"/legal/terms_and_conditions/", kelimart.InfoHand.TermsConditions),
-            (r"/report_a_counterfeit/", kelimart.InfoHand.Counterfeit),
-            (r"/product/.*", kelimart.ProductHand.handler),
-            (r"/refresh_cart/", kelimart.RefreshCartAjax.handler),
-            (r"/sign_in/reset_password/", kelimart.ResetPWHand.handler),
-            (r"/sign_in/", kelimart.SignInHand.handler),
-            (r"/sign_up/", kelimart.SignUpHand.handler),
-            (r"/test/", kelimart.TestingHand.handler),
-            (r"/verify/", kelimart.VerifyHand.handler),
-            (r"/.*", kelimart.InfoHand.NotFound)
-        ]),
-        (HostMatches("www.kelimart.com"), [
-            (r"/.*", RedirectRemoveWWW)
-        ]),
         (HostMatches("franzar.com"), [
-            (r"/", franzar.F_HomeHand.handler)
+            (r"/about_us/", franzar.AboutHand.handler),
+            (r"/account/", franzar.AccountHand.handler),
+            (r"/add_to_cart/", franzar.AddToCartAjax.handler),
+            (r"/cart/", franzar.CartHand.handler),
+            (r"/account/locales/", franzar.ChangeLocalesAjax.handler),
+            (r"/contact/", franzar.ContactHand.handler),
+            (r"/sign_in/forgot_password/", franzar.ForgotPWHand.handler),
+            (r"/", franzar.HomeHand.handler),
+            (r"/legal/terms_and_conditions/", franzar.InfoHand.TermsConditions),
+            (r"/report_a_counterfeit/", franzar.InfoHand.Counterfeit),
+            (r"/product/.*", franzar.ProductHand.handler),
+            (r"/refresh_cart/", franzar.RefreshCartAjax.handler),
+            (r"/sign_in/reset_password/", franzar.ResetPWHand.handler),
+            (r"/sign_in/", franzar.SignInHand.handler),
+            (r"/sign_up/", franzar.SignUpHand.handler),
+            (r"/test/", franzar.TestingHand.handler),
+            (r"/verify/", franzar.VerifyHand.handler),
+            (r"/.*", franzar.InfoHand.NotFound)
         ]),
         (HostMatches("www.franzar.com"), [
-            (r"/.*", RedirectRemoveWWW)
+            (r"/.*", RedirectRemoveWWW_Franzar)
+        ]),
+        (HostMatches("kelimart.com"), [
+            (r"/", kelimart.HomeHand.handler)
+        ]),
+        (HostMatches("www.kelimart.com"), [
+            (r"/.*", RedirectRemoveWWW_Kelimart)
         ])
     ], **settings)
 
