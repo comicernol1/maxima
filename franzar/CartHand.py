e040e0398b243b25b7ab3d6bb3ff4c9e9e9ef0b5
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-24 22:06:13 -0400

    Update CartHand.py

--- franzar/CartHand.py
+++ franzar/CartHand.py
@@ -18,7 +18,7 @@ class handler(tornado.web.RequestHandler):
             else:
                 UserCartItem_PriceSet = "{0:s}{1:,.2f}".format(UserCurrencySymbol,UserCartItem_Price)
             if str(UserCartItem_ID)[0:7] != "1111111":
-                UserCartItem_TemplateSet = "<select class=\"CIp\"><option>Master Crafted</option><option>X-Small</option><option>Small</option><option>Medium</option></select><option>Large</option><option>X-Large</option></select>"
+                UserCartItem_TemplateSet = "<select class=\"CIp\"><option>Master Crafted</option><option>X-Small</option><option>Small</option><option>Medium</option><option>Large</option><option>X-Large</option></select>"
             else:
                 UserCartItem_TemplateSet = ""
             if str(UserCartItem_ID)[0:7] == "1111111":
