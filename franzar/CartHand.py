609d37300c0ca6f3e7091726f6213e0f89958cc4
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-25 01:57:57 -0400

    Update CartHand.py

--- franzar/CartHand.py
+++ franzar/CartHand.py
@@ -29,7 +29,7 @@ class handler(tornado.web.RequestHandler):
                 UserCartAdjustNumDisable = ""
                 UserCartRemoveButton = "<button class=\"CIr\" onclick=\"RMp('{0:d}')\">Remove</button>".format(UserCartItem_ID)
             UserCartListTotal += (UserCartItem_Price*int(UserCartList[i][1]))
-            UserCartItems += "<div class=\"CIt\" id=\"CIt_{0:d}\" prc=\"{8:f}\" style=\"top:{1:d}px;\"><input class=\"CIq\" type=\"number\" value=\"{2:d}\" onblur=\"AdjOa()\"{3:s}><a href=\"/product/{0:d}/\" class=\"CIi\" style=\"background-image:url({4:s});\"></a>{5:s}<h3>{6:s}</h3><h1>{7:s}</h1>{9:s}</div>\n".format(UserCartItem_ID,(i*210),int(UserCartList[i][1]),UserCartAdjustNumDisable,UserCartItem_ImgLink,UserCartItem_TemplateSet,FindProduct(UserCartItem_ID)["Name"],UserCartItem_PriceSet,UserCartItem_Price,UserCartRemoveButton)
+            UserCartItems += "<div class=\"CIt\" id=\"CIt_{0:d}\" prc=\"{8:f.2}\" style=\"top:{1:d}px;\"><input class=\"CIq\" type=\"number\" value=\"{2:d}\" onblur=\"AdjOa()\"{3:s}><a href=\"/product/{0:d}/\" class=\"CIi\" style=\"background-image:url({4:s});\"></a>{5:s}<h3>{6:s}</h3><h1>{7:s}</h1>{9:s}</div>\n".format(UserCartItem_ID,(i*210),int(UserCartList[i][1]),UserCartAdjustNumDisable,UserCartItem_ImgLink,UserCartItem_TemplateSet,FindProduct(UserCartItem_ID)["Name"],UserCartItem_PriceSet,UserCartItem_Price,UserCartRemoveButton)
         if UserCartItems != "":
             if UserCurrencySymbol in SpecifyCurrencyList:
                 UserCartTotalsSet = "<h3 id=\"CICt\">Total: {0:s}<u id=\"CICTt\">{1:,.2f}</u> ({2:s})</h3>".format(UserCurrencySymbol,UserCartListTotal,UserCurrency)
