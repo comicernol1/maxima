d14527b581062dab58bae529566b9c86939b2998
comicernol1 <59543823+comicernol1@users.noreply.github.com>
2020-04-26 22:33:46 -0400

    Update index.py

--- index.py
+++ index.py
@@ -44,6 +44,7 @@ if __name__ == "__main__":
             (r"/sign_in/forgot_password/", franzar.ForgotPWHand.handler),
             (r"/", franzar.HomeHand.handler),
             (r"/legal/terms_and_conditions/", franzar.InfoHand.TermsConditions),
+            (r"/legal/privacy_policy/", franzar.InfoHand.PrivacyPolicy),
             (r"/report_a_counterfeit/", franzar.InfoHand.Counterfeit),
             (r"/deliveries_and_returns/", franzar.InfoHand.Returns),
             (r"/payments/", franzar.InfoHand.Payments),
